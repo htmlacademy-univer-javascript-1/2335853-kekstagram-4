5af8775fee8cd0f21338a3096257282051d473c9
Keks <keks@htmlacademy.ru>
2023-11-27 22:33:35 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/pictures.js
@@ -0,0 +1,34 @@
+import { createPosts } from './data.js';
+import { showBigPicture } from './big-picture.js';
+
+const pictureList = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const renderPicture = (post) => {
+  const newPicture = pictureTemplate.cloneNode(true);
+
+  newPicture.querySelector('.picture__img').src = post.url;
+  newPicture.querySelector('.picture__img').alt = post.description;
+  newPicture.querySelector('.picture__likes').textContent = post.likes;
+  newPicture.querySelector('.picture__comments').textContent = post.comments.length;
+
+  newPicture.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    showBigPicture(post);
+  });
+
+  return newPicture;
+};
+
+const renderPictures = () => {
+  const fragment = document.createDocumentFragment();
+  const generatedData = createPosts();
+
+  generatedData.forEach((post) => {
+    fragment.appendChild(renderPicture(post));
+  });
+
+  pictureList.appendChild(fragment);
+};
+
+export { renderPictures };
---
+++ 8/js/util.js
@@ -0,0 +1,8 @@
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const getRandomArrayElement = (list) => list[getRandomInteger(0, list.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+
+export { getRandomInteger, getRandomArrayElement, isEscapeKey };
